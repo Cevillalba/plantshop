145fa24db45f440e15ffbe69640205665ad4cd88
Cevillalba <111022981+Cevillalba@users.noreply.github.com>
2025-11-26 12:32:05 -0500

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 🔑 LÍNEA CLAVE: El nombre de tu repositorio es 'plantshop'
-  base: '/plantshop/', 
-});
+  base: '/plantshop/'   // MUY IMPORTANTE
+})
